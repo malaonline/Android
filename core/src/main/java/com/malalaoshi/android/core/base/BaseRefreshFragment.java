de4b43f6b053965b85cdfb013a2169de61bbb762
Donald <yandebo@malalaoshi.com>
2017-04-26 10:54:35 +0800

    add toast when network error

--- core/src/main/java/com/malalaoshi/android/core/base/BaseRefreshFragment.java
+++ core/src/main/java/com/malalaoshi/android/core/base/BaseRefreshFragment.java
@@ -251,11 +251,10 @@ public abstract class BaseRefreshFragment<T extends BaseResult> extends BaseFrag
     protected void refreshFinish(T response) {
         refreshLayout.refreshComplete();
         if (response == null) {
+            MiscUtil.toast(R.string.network_error);
             if(adapter.getItemCount()<=0){
                 setLayout(LayoutType.REFRESH_FAILED);
 //                setErrorViewText(getErrorString());
-            }else {
-                MiscUtil.toast(R.string.network_error);
             }
             return;
         }
