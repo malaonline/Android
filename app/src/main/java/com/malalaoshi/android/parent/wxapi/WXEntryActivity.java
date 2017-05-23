912f6eb109dbff3258e4bf5840923210dec362bd
Donald <yandebo@malalaoshi.com>
2017-05-23 18:06:08 +0800

    change position of wx

--- app/src/main/java/com/malalaoshi/android/parent/wxapi/WXEntryActivity.java
+++ app/src/main/java/com/malalaoshi/android/parent/wxapi/WXEntryActivity.java
@@ -6,7 +6,7 @@
  * Copyright (c) 2013年 mob.com. All rights reserved.
  */
 
-package com.malalaoshi.android.activitys.wxapi;
+package com.malalaoshi.android.parent.wxapi;
 
 import android.content.Intent;
 import android.widget.Toast;
